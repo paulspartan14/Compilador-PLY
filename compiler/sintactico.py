79e867c08e3de26b33fb9006946b2d1bbdc64770
Paul Mena <e17081416@itmerida.edu.mx>
2020-10-01 19:58:35 +0000

    remove prints

--- compiler/sintactico.py
+++ compiler/sintactico.py
@@ -26,35 +26,35 @@ class Sintactic(object):
     def p_start_prod_or_sep(self, p):
         ''' S : production S 
                 | production SEP1 S '''
-        print("tengo ;")
+        #print("tengo ;")
     pass
 
     def p_start_only_prod(self, p):
         ' S : production '
-        print("soy produccion")
+        #print("soy produccion")
         pass
 
     # PRODUCTIONS #
     def p_production_decl_sep(self, p):
         ' production : declaracion SEP1 '
-        print("estoy entrando a declaracion SEP1")
+        #print("estoy entrando a declaracion SEP1")
         pass
 
     def p_expressions(self, p):
         ' production : expression SEP1 '
-        print("entro a expresion ;")
+        #print("entro a expresion ;")
         pass
 
     def p_functions(self, p):
         ' production : function '
-        print('funcion')
+        #print('funcion')
         pass
 
     # ITERATORS #
 
     def p_iterators(self, p):
         ' production : iterators '
-        print('iterador')
+        #print('iterador')
         pass
 
     # DECLARATIONS #
@@ -76,7 +76,7 @@ class Sintactic(object):
             p[0] = p[4]
         
         def type_incorrecto():
-            print('Incompatible Types')
+            #print('Incompatible Types')
             self.errsemcount += 1
             self.errors.append({
                 'line': p.lineno(2),
@@ -92,7 +92,7 @@ class Sintactic(object):
         if p[1] == 'int':
             # evalua si es numero entero
             if isinstance(p[4], int):
-                print(f'variable INT declarada')
+                #print(f'variable INT declarada')
                 td = int(p[4])
                 type_correcto()                
             else:
@@ -101,7 +101,7 @@ class Sintactic(object):
         elif p[1] == 'float':
             # evalua si es numero float
             if isinstance(p[4], float):
-                print(f'variable FLOAT declarada')
+                #print(f'variable FLOAT declarada')
                 td = float(p[4])
                 type_correcto()
             else:
@@ -113,7 +113,7 @@ class Sintactic(object):
 
         elif p[1] == 'string':            
             if isinstance(p[4], str):
-                print(f'variable STR declarada')
+                #print(f'variable STR declarada')
                 td = str(p[4])
                 type_correcto()
             else:
@@ -122,7 +122,7 @@ class Sintactic(object):
     #ej: in3t a = 12;  toma in3t como variable y no como type y a como id
     def p_var_declarations_error(self, p):
         ' declaracion : ID ID AS1 expression '
-        print('1.-ERROR DE REDECLARACION ID:', p[1])
+        #print('1.-ERROR DE REDECLARACION ID:', p[1])
         self.errors.append({
             'line': p.lineno(1),
             'value': p[1],
@@ -157,9 +157,9 @@ class Sintactic(object):
         ' expression : ID '
         try:
             p[0] = self.names[p[1]]['value']
-            print(self.names[p[1]]['value'])
+            #print(self.names[p[1]]['value'])
         except LookupError:
-            print(f"Variable {p[1]!r} no definida")
+            #print(f"Variable {p[1]!r} no definida")
             self.errsemcount += 1
             self.errors.append({
                 'line': p.lineno(1),
@@ -176,9 +176,9 @@ class Sintactic(object):
 
         try:
             self.names[p[1]]['value'] = p[3]
-            print("variable redefinida")
+            #print("variable redefinida")
         except LookupError:
-            print(f"Variable {p[1]!r} no definida")
+            #print(f"Variable {p[1]!r} no definida")
             self.errsemcount += 1
             self.errors.append({
                 'line': p.lineno(1),
@@ -210,7 +210,7 @@ class Sintactic(object):
     # error de id duplicada
     def p_function_error(self, p):
         ' function : ID ID DEL1 argv DEL2 DEL3 S DEL4'
-        print(p[1], p[2])
+        #print(p[1], p[2])
         self.functions[p[2]] = 0
 
     def p_argv(self, p):
@@ -247,7 +247,7 @@ class Sintactic(object):
     # ITERADORES
     def p_whileStmt(self, p):
         ' iterators : IT1 DEL1 expr DEL2 DEL3 S DEL4 '
-        print(p[1])
+        #print(p[1])
 
     def p_expr(self, p):
         'expr : expr_rec'
@@ -285,7 +285,7 @@ class Sintactic(object):
     # errores
 
     def p_error(self, p):
-        print(f"Unespected token '{p.value}'")  
+        #print(f"Unespected token '{p.value}'")  
         self.errsint +=1
         self.errors.append({
             'line': p.lineno,
@@ -314,8 +314,8 @@ if __name__ == "__main__":
         program = f.read()
         f.close
         ERR, NM = Sintactic().compile(program)
-        print(f"\n{NM}\n") # imprime nombres con valores
-        print(ERR) # imprime errores
+        #print(f"\n{NM}\n") # imprime nombres con valores
+        #print(ERR) # imprime errores
     except IndexError:
         while True:
             try:
@@ -323,79 +323,3 @@ if __name__ == "__main__":
             except EOFError:
                 break
             Sintactic().parser.parse(s)
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''precedence = (
-    ('right', 'AS'),
-    ('left', 'PLUS', 'MINUS'),
-    ('left', 'TIMES', 'DIVIDE'),
-    ('right', 'EQUALS'),
-    ('left', 'LPARENT', 'RPARENT') 
-    # en la precedencia se mira de abajo hacia arria
-    # abajo es el mas importante, arriba menos importante
-)
-'''
-'''
-
-    Rule 0       program -> S
-    Rule 1       S -> produccion S
-    Rule 2       S -> produccion END_LINE S
-    Rule 3       S -> produccion
-    Rule 4       produccion -> declaracion END_LINE
-    Rule 5       produccion -> expresion END_LINE
-    Rule 6       produccion -> funcion
-    Rule 7       declaracion -> TD1 ID AS1 expresion
-    Rule 8       expresion -> expresion OPAR1 expresion --
-    Rule 9       expresion -> DEL1 expresion DEL2
-    Rule 10      expresion -> CNE
-    Rule 11      expresion -> <empty> --
-    Rule 12      expresion -> ID ---
-    Rule 13      expresion -> ID AS1 expresion
-    Rule 14      funcion -> TD1 ID DEL1 argumento DEL2 DEL3 S DEL4
-    Rule 15      argumento -> argumento_dec
-    Rule 16      argumento -> <empty>
-    Rule 17      argumento_dec -> TD1 ID SEP1 argumento_dec
-    Rule 18      argumento_dec -> T1 ID
-
-    falta iteradores
-
-    program = block
-    block = const-decl var-decl proc-decl statement
-    const-decl = CONST const-asigment-list | e
-
- '''
-# sentencias #
-# Producciones #
-# no terminales, terminales y producciones vacias #
-
